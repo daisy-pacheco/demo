52615558b337d1c9672ae1237e29164d6f4a1ab5
Jackson Hoffart <jackson.hoffart@gmail.com>
2020-06-23 14:07:52 +0200

    export BAD add() function

---
+++ R/add.R
@@ -0,0 +1,21 @@
+#' Add two values
+#'
+#' `add` returns the sum of it's two arguments.
+#'
+#' @param a numeric value
+#' @param b numeric value
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' add(2,2)
+#'
+#' add(-1,5)
+#'
+add <- function(a,b){
+
+  stopifnot(is.numeric(a),is.numeric(b))
+
+  a - b
+}
